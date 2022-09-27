8de4372be870627291e5cee52e1c058966a29994
dmlrpfl <dmlrpfl@gmail.com>
2022-09-27 11:40:47 +0900

    스페셜챌린지 작업중

--- src/resources/js/balro.js
+++ src/resources/js/balro.js
@@ -8,3 +8,5 @@ window.addEventListener('resize', () => {
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+import {Swiper} from './module/swiper-bundle.esm.browser.min.js';
