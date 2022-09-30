12a90d7afee9780d843e4128067fd3ce0eefcf27
dmlrpfl <dmlrpfl@gmail.com>
2022-09-30 12:06:37 +0900

    스페셜 챌린지

--- src/resources/js/balso.js
+++ src/resources/js/balso.js
@@ -15,12 +15,8 @@ const modalBtnClose = document.querySelectorAll('.modal__close');
 modalBtnClose.forEach(item => {
 	item.addEventListener('click', () => {
 		item.closest('.modal').style.display = 'none';
-		document.querySelector('.scrolllock').classList.remove('scrolllock');
+		document.body.classList.remove('scrolllock');
 	});
 });
-
-// setTimeout(() => {
-// 	document.getElementsByClassName('modal')[0].style.display = 'block';
-// }, 2000);
 
 import {Swiper} from './module/swiper-bundle.esm.browser.min.js';
